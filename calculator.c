d17b5aa4eb987b3ed1acc62a81b2c62c757d8227
AnSwErYWJ <1045837697@qq.com>
2015-06-07 15:59:54 +0800

    V3

--- calculator.c
+++ calculator.c
@@ -20,7 +20,8 @@ void input(double *num1,double *num2,char *operation)
     printf("Your input:\n");
     scanf("%lf%c%lf",num1,operation,num2);
     //fflush(stdin);//gcc不支持这个函数
-    getchar();
+    while(getchar() != '\n')
+    continue;
 }
 
 /*计算函数*/
@@ -30,30 +31,30 @@ void calc_output(const double num1,const double num2,const char operation)
     switch(operation)
     {
         case '+':
-            printf("Result:\n%lf + %lf = %lf\n",num1,num2,num1+num2);
-            break;
+        printf("Result:\n%lf + %lf = %lf\n",num1,num2,num1+num2);
+        break;
         case '-':
-            printf("Result:\n%lf - %lf = %lf\n",num1,num2,num1-num2);
-            break;
+        printf("Result:\n%lf - %lf = %lf\n",num1,num2,num1-num2);
+        break;
         case '*':
-            printf("Result:\n%lf * %lf = %lf\n",num1,num2,num1*num2);
-            break;
+        printf("Result:\n%lf * %lf = %lf\n",num1,num2,num1*num2);
+        break;
         case '/':
-            if(num2 == 0)
-                printf("\aError:Davision can not be zero!\n");
-            else
-                printf("Result:\n%lf / %lf = %lf\n",num1,num2,num1/num2);
-            break;
+        if(num2 == 0)
+        printf("\aError:Davision can not be zero!\n");
+        else
+        printf("Result:\n%lf / %lf = %lf\n",num1,num2,num1/num2);
+        break;
         /*%c操作只能对整数进行，所以类型转换为long*/
         case '%':
-            if((long)num2 == 0)
-                printf("\aError:Davision can not be zero!\n");
-            else
-                printf("Result:\n%ld %% %ld = %ld\n",(long)num1,(long)num2,(long)num1%(long)num2);
-            break;
+        if((long)num2 == 0)
+        printf("\aError:Davision can not be zero!\n");
+        else
+        printf("Result:\n%ld %% %ld = %ld\n",(long)num1,(long)num2,(long)num1%(long)num2);
+        break;
         default:
-            printf("Usage:Input should be as num1operationnum2.\n");
-            break;
+        printf("Usage:Input should be as num1operationnum2.\n");
+        break;
     }
 }
 int main(int argc,char *argv[])
@@ -70,21 +71,22 @@ int main(int argc,char *argv[])
         printf("Please enter your choice(c or q):");
         fflush(stdin);
         scanf("%c",&choice);
-        getchar();
+        while(getchar() != '\n')
+        continue;
         switch(choice)
         {
             case 'c':
-                input(&num1,&num2,&operation);
-                calc_output(num1,num2,operation);
-                break;
+            input(&num1,&num2,&operation);
+            calc_output(num1,num2,operation);
+            break;
             case 'q':
-                printf("-------------------------------\n");
-                printf("\n#################################################\n");
-                printf("Bye!\n\n");        
-                exit(0);
+            printf("-------------------------------\n");
+            printf("\n#################################################\n");
+            printf("Bye!\n\n");        
+            exit(0);
             default:
-                printf("Error:Your choice is error!\n");
-                break;
+            printf("Error:Your choice is error!\n");
+            break;
         }
         printf("-------------------------------\n");
     }
